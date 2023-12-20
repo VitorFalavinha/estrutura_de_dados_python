3cef46eff5e4a1c87a41bfd2e775e50d112b82e7
VitorFalavinha <135779845+VitorFalavinha@users.noreply.github.com>
2023-12-20 09:27:32 -0300

    Update sorteio_e_soma.py

--- Estrutura+de+Dados+e+Algoritmos+com+Python/Estrutura de Dados e Algoritmos com Python/exercicios/sorteio_e_soma.py
+++ Estrutura+de+Dados+e+Algoritmos+com+Python/Estrutura de Dados e Algoritmos com Python/exercicios/sorteio_e_soma.py
@@ -2,15 +2,24 @@ from random import randint
 from time import sleep
 
 def sorteia(lista):
+    print('Sorteando 5 valores da lista: ')
     for cont in range(0, 5):
         n = randint(1, 10)
         lista.append(n)
         print(f'{n} ', end='')
         sleep(0.3)
 
-#def somaPar():
+def somaPar(lista):
+    print('Somando os valores pares da lista: ')
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'A soma doa pares da lista {lista} temos: ', soma)
+
 
 numeros = list()
 sorteia(numeros)
+somaPar(numeros)
 
 
