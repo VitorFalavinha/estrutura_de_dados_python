5da65cb2853b12e33ffc3d3f0110f7a47a443860
VitorFalavinha <135779845+VitorFalavinha@users.noreply.github.com>
2023-12-20 17:06:00 -0300

    Update DESAFIO.py

--- Estrutura+de+Dados+e+Algoritmos+com+Python/Estrutura de Dados e Algoritmos com Python/exercicios/DESAFIO.py
+++ Estrutura+de+Dados+e+Algoritmos+com+Python/Estrutura de Dados e Algoritmos com Python/exercicios/DESAFIO.py
@@ -1,12 +1,49 @@
 import numpy as np
+import pandas as pd
 
 
-def deletar(*matriz):
+def contar_aprovados(matrix):
+    lin = matriz.shape[0]
+    col = matriz.shape[1]
+    aprovados = reprovados = ausentes = 0
+
+    for i in range(0, lin):
+        for j in range(0, col):
+            if matriz[i][j] == "442002106":
+                matriz[i][j] = ('VITOR FALAVINHA')
+
+    for i in range(0, lin):
+        for j in range(0, col):
+            if matriz[i][j] == 'APROVADO':
+                print(matriz[i])
+                aprovados += 1
+
+    print('Quantidade de aprovados: ', aprovados)
+
+    for i in range(0, lin):
+        for j in range(0, col):
+            if matriz[i][j] == 'REPROVADO':
+                print(matriz[i])
+                reprovados += 1
+
+    print('Quantidade de reprovados: ', reprovados)
+    for i in range(0, lin):
+        for j in range(0, col):
+            if matriz[i][j] == "AUSENTE":
+                print(matriz[i])
+                ausentes += 1
+
+    print('Quantidade de ausentes: ', ausentes)
+
+
+def ordenar(matrix):
+    df = pd.DataFrame(matriz)
+    df.sort_values([5], inplace=True, ascending=False)
+    pd.set_option('display.max_rows', None)
+    print(df)
 
 
 matriz = np.array([[311, 'PROFESSOR II - INGLÊS', 442000020, 0, 0, 0, 'AUSENTE'],
-                   [311, 'PROFESSOR II - INGLÊS', 442000085, 4, 26, 30, 'APROVADO'],
-                   [311, 'PROFESSOR II - INGLÊS', 442000085, 4, 26, 30, 'APROVADO'],
                    [311, 'PROFESSOR II - INGLÊS', 442000085, 4, 26, 30, 'APROVADO'],
                    [311, 'PROFESSOR II - INGLÊS', 442000280, 4, 16, 20, 'REPROVADO'],
                    [311, 'PROFESSOR II - INGLÊS', 442000432, 4, 25, 29, 'REPROVADO'],
@@ -91,5 +128,35 @@ matriz = np.array([[311, 'PROFESSOR II - INGLÊS', 442000020, 0, 0, 0, 'AUSENTE'
                    [311, 'PROFESSOR II - INGLÊS', 442005787, 0, 0, 0, 'AUSENTE'],
                    [311, 'PROFESSOR II - INGLÊS', 442005825, 2, 12, 14, 'REPROVADO']])
 
+matrizDissertativa = np.array([[442000085, 311, 'PROFESSOR II - INGLÊS', 61, 68, 0, 43.0, 'REPROVADO'],
+                               [442000829, 311, 'PROFESSOR II - INGLÊS', 52, 16, 34, 34.0, 'REPROVADO'],
+                               [442001284, 311, 'PROFESSOR II - INGLÊS', 59, 48, 63, 56.7, 'REPROVADO'],
+                               [442001925, 311, 'PROFESSOR II - INGLÊS', 79, 83, 58, 73.3, 'APROVADO'],
+                               [442002106, 311, 'PROFESSOR II - INGLÊS', 62, 89, 87, 79.3, 'APROVADO'],
+                               [442002124, 311, 'PROFESSOR II - INGLÊS', 61.5, 74, 65, 66.8, 'APROVADO'],
+                               [442002234, 311, 'PROFESSOR II - INGLÊS', 47, 84, 71, 67.3, 'APROVADO'],
+                               [442002502, 311, 'PROFESSOR II - INGLÊS', 77, 52, 66, 65.0, 'APROVADO'],
+                               [442002541, 311, 'PROFESSOR II - INGLÊS', 59, 66, 57, 60.7, 'APROVADO'],
+                               [442002542, 311, 'PROFESSOR II - INGLÊS', 86, 66, 81, 77.7, 'APROVADO'],
+                               [442002606, 311, 'PROFESSOR II - INGLÊS', 90, 81, 73, 81.3, 'APROVADO'],
+                               [442003051, 311, 'PROFESSOR II - INGLÊS', 91, 59, 75, 75.0, 'APROVADO'],
+                               [442003054, 311, 'PROFESSOR II - INGLÊS', 70, 79, 83, 77.3, 'APROVADO'],
+                               [442003194, 311, 'PROFESSOR II - INGLÊS', 87, 61, 66, 71.3, 'APROVADO'],
+                               [442003507, 311, 'PROFESSOR II - INGLÊS', 66, 52, 55, 57.7, 'REPROVADO'],
+                               [442003853, 311, 'PROFESSOR II - INGLÊS', 64, 49, 54, 55.7, 'REPROVADO'],
+                               [442004411, 311, 'PROFESSOR II - INGLÊS', 79, 76, 44, 66.3, 'APROVADO '],
+                               [442004604, 311, 'PROFESSOR II - INGLÊS', 55, 76, 70, 67.0, 'APROVADO '],
+                               [442004902, 311, 'PROFESSOR II - INGLÊS', 57, 48, 42, 49.0, 'REPROVADO'],
+                               [442005010, 311, 'PROFESSOR II - INGLÊS', 84, 79, 43, 68.7, 'APROVADO '],
+                               [442005204, 311, 'PROFESSOR II - INGLÊS', 89, 64, 89, 80.7, 'APROVADO '],
+                               [442005244, 311, 'PROFESSOR II - INGLÊS', 66, 60, 78, 68.0, 'APROVADO '],
+                               [442005319, 311, 'PROFESSOR II - INGLÊS', 73, 0, 0, 24.3, 'REPROVADO '],
+                               [442005322, 311, 'PROFESSOR II - INGLÊS', 53, 58, 67, 59.3, 'REPROVADO '],
+                               [442005519, 311, 'PROFESSOR II - INGLÊS', 77, 66, 80, 74.3, 'APROVADO']])
 
-deletar(matriz)
+#column_names = ["cod", "cargo", "inscricao", "n1", "n2", "n3", "situation"]
+#df = pd.DataFrame(matriz, columns=column_names)
+
+#contar_aprovados(matriz)
+#somar_notas(matriz, matrizDissertativa)
+#ordenar(matriz)
