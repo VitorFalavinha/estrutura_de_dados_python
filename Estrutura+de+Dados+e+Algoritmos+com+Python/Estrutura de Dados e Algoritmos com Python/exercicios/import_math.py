b59db722abcf9a486bd47459ee2450cc684d0f96
VitorFalavinha <135779845+VitorFalavinha@users.noreply.github.com>
2023-12-20 17:16:41 -0300

    libraries

---
+++ Estrutura+de+Dados+e+Algoritmos+com+Python/Estrutura de Dados e Algoritmos com Python/exercicios/import_math.py
@@ -0,0 +1,20 @@
+import math
+
+math.sqrt() RAIZ QUADRADA
+
+math.sin() SENO
+
+math.cos() COSENO
+
+math.log() LOGARITMO
+
+math.e()  NUMERO DE EULER
+
+math.pi() NUMERO DE PI
+
+import datetime
+
+dir(datetime) MOSTRA RECURSOS DA BIBLIOTECA
+
+datetime.date.today()
+datetime.datetime.now()
