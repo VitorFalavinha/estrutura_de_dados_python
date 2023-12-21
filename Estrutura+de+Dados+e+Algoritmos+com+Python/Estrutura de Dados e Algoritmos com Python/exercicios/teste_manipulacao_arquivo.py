346c87d200c170eb8c624bce07076829e73f11ec
VitorFalavinha <135779845+VitorFalavinha@users.noreply.github.com>
2023-12-21 13:16:14 -0300

    manipulação de arquivos

---
+++ Estrutura+de+Dados+e+Algoritmos+com+Python/Estrutura de Dados e Algoritmos com Python/exercicios/teste_manipulacao_arquivo.py
@@ -0,0 +1,8 @@
+with open('C:/Users/vitor/Documents/Estrutura de dados e algoritimos em Python\estrutura_de_dados_python/Estrutura+de+Dados+e+Algoritmos+com+Python/Estrutura de Dados e Algoritmos com Python/texto_teste.txt') as tex:
+    r = tex.readlines()
+    print(r)
+
+with open('texto.txt2','w') as texto:
+    texto.write('Ola a todos')
+    
+    print(txto2.txt)
