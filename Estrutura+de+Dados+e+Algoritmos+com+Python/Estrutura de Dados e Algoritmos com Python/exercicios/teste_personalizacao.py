9324c6e3ee36c9b47a0dbd43ab19db9a81a381d3
VitorFalavinha <135779845+VitorFalavinha@users.noreply.github.com>
2023-12-20 22:40:29 -0300

    modulos personalizados

---
+++ Estrutura+de+Dados+e+Algoritmos+com+Python/Estrutura de Dados e Algoritmos com Python/exercicios/teste_personalizacao.py
@@ -0,0 +1,9 @@
+import utilitarios as ut
+
+ut.soma(1,2,3)
+
+ut.mult(3,2,2)
+
+ut.isPalindromo('arara')
+
+ut.divisao(4,2)
---
+++ Estrutura+de+Dados+e+Algoritmos+com+Python/Estrutura de Dados e Algoritmos com Python/utilitarios.py
@@ -0,0 +1,21 @@
+def soma(a, b, c):
+    somatorio = a+b+c
+    print(somatorio)
+    return somatorio
+
+def mult(a,b,c):
+    produto = a*b*c
+    print(produto)
+    return produto
+
+def isPalindromo(string):
+    if string == string[::-1]:
+        print('É palindromo')
+        return True
+    else:
+        print('Não é palindromo')
+        return False
+
+def divisao(a,b):
+    print(a/b)
+    return a/b
